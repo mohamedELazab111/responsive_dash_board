ce89a71d82c31c657c8113b47aeea056c3e43370
mohamedELazab111 <mobr876@gmail.com>
2025-09-17 21:15:24 +0300

    add drawer

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(drawer: CustomDrawer(), body: HomeViewBody());
   }
 }
