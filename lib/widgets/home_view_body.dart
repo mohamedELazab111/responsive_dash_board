ea46ee6a917deb877421f089d896070268be6a5f
mohamedELazab111 <mobr876@gmail.com>
2025-10-13 22:19:19 +0300

    add dashboard layout and styles

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adabtive_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return AdaptiveLayout(
+      mobileLayout: (context) => const SizedBox(),
+      tabletLayout: (context) => const SizedBox(),
+      desktopLayout: (context) => const DashboardDesktopLayout(),
+    );
   }
 }
